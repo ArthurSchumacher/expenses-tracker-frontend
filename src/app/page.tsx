df0bf67efb0be2653ac2e3c3ef4a429e83b36876
Arthur Schumacher <arthur.s.cunha00@gmail.com>
2024-04-19 12:29:09 -0400

    feat: Changed to min-h-screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SignInForm from "@/components/SignInForm";
 
 export default function Home() {
   return (
-    <main className="grid items-center justify-center h-screen">
+    <main className="grid items-center justify-center min-h-screen">
       <SignInForm />
     </main>
   );
